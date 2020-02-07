e17979d9c1033ba6b12cea1274c95386a7171ee1
Philip Kristoffersen <philipkristoffersen@gmail.com>
2020-02-07 04:45:45 -0800

    Inital matrix grid.

--- src/web/src/components/App.tsx
+++ src/web/src/components/App.tsx
@@ -4,13 +4,69 @@ export interface AppProps {
 }
 
 export interface AppState {
+    players: Player[],
+    tileMatrix: TileType[][]
+}
+
+enum TileType {
+    Empty = 0,
+    Wall = 1
+}
+
+interface Position {
+    x: number;
+    y: number;
 
 }
 
-const FunctionApp: React.FunctionComponent<AppProps> = () => <div>Hello World</div>;
+interface Player {
+    id: string;
+    position: Position;
+}
+
+interface MapResult {
+    players: Player[],
+    tileMatrix: TileType[][]
+}
+
+const ClassNames = {
+    0: "Empty",
+    1: "Wall"
+}
+
+const apiUrl = "http://10.204.26.143:5000/api/map";
 
 export default class App extends React.Component<AppProps, AppState> {
+    constructor(props: AppProps) {
+        super(props);
+        this.fetchData = this.fetchData.bind(this);
+        this.state = { players: [], tileMatrix: [] };
+    }
     render() {
-        return <h1>Hello World</h1>;
+
+        const rows = this.state.tileMatrix;
+        const matrix = rows.map(row => {
+
+            const columnTds = row.map(column => {
+                const className = ClassNames[column];
+                return < td className={`column ${className}`} />;
+            })
+            return <tr>{columnTds}</tr>;
+
+        })
+        return <table>{matrix}</table>;
+    }
+
+    componentDidMount() {
+        setInterval(this.fetchData, 100);
+    }
+
+    fetchData() {
+        fetch(apiUrl).then(response => response.json()).then(json => json as MapResult).then(mapResult => {
+            this.setState({
+                players: mapResult.players,
+                tileMatrix: mapResult.tileMatrix
+            })
+        });
     }
 }
